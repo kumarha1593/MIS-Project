5b7dfa2cfebb3282fc0a8fcc92aac9460b818459
AnshikaJaiswal28 <jaiswalanshika2000@gmail.com>
2024-09-07 18:21:11 +0530

    added logo

--- frontend/src/components/AppLayout/AppLayout.js
+++ frontend/src/components/AppLayout/AppLayout.js
@@ -1,12 +1,13 @@
 import React from "react";
 import UserMenu from "../UserMenu/UserMenu";
 import "./AppLayout.css";
+import logo from "../../assests/logo.png";
 
 const AppLayout = ({ children }) => {
   return (
     <div className="app-layout">
       <header className="app-header">
-        <img src="/logo.png" alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="logo" />
         <UserMenu />
       </header>
       <main className="app-main">{children}</main>
