70cd8446a82a6461fb771fcb0d5d5b69ce893953
Mumuksh <mumuksh7@gmail.com>
2024-09-22 02:29:06 +0530

    build file update

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "$Mumuksh14$",
-  database: "user_management",
+  database: "manipur",
 });
 
 db.connect((err) => {
@@ -2973,7 +2973,7 @@ app.post("/api/final-submit", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
